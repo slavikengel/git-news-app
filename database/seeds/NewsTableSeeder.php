14e18e6d6921a37beea8514e98ff70db9f00f05f
Slavik Engel <Engel.palych@gmail.com>
2017-05-11 09:26:18 +0400

    add NewsTableSeeder.php

--- database/seeds/NewsTableSeeder.php
+++ database/seeds/NewsTableSeeder.php
@@ -1,10 +1,9 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\News;
 
-use App\Models\User;
-
-class UsersTableSeeder extends Seeder
+class NewsTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
@@ -13,6 +12,6 @@ class UsersTableSeeder extends Seeder
      */
     public function run()
     {
-        factory(User::class, 1)->create();
+        factory(News::class, 1)->create();
     }
 }
